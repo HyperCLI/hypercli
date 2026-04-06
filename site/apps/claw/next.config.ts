fa9bb546c01ee7856e6cdc2c2f180b4aaaca2237
Nishan Poudel <unseenguy42@gmail.com>
2026-04-06 10:09:27 +0545

    Altered next.config.ts to avoid local runtime errors.

--- site/apps/claw/next.config.ts
+++ site/apps/claw/next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 import path from "path";
 
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const requiredEnvVars = [
   "NEXT_PUBLIC_MAIN_SITE_URL",
   "NEXT_PUBLIC_CONSOLE_URL",
